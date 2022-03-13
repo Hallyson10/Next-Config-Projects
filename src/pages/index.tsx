d339f8454048c661fd0f2c88d1deba3f22e8bcd4
Hallyson Miranda <hallyson.miranda@insightlab.ufc.br>
2022-03-13 01:48:17 -0300

    Add Test Jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
@@ -12,5 +12,4 @@ const Home: NextPage = () => {
     </div>
   )
 }
-
 export default Home
